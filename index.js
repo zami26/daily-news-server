309326553e697a9bc35628f8979b5244ca0518c6
zami26 <arafatulislamzami@gmail.com>
2023-06-23 10:51:07 +0600

    last commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 app.get('/', (req, res) => {
@@ -12,6 +13,27 @@ app.get('/', (req, res) => {
 
 app.get('/data-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '8') {
+        res.send(news);
+    }
+    else {
+        const category = news.filter(n => n.category_id === id);
+        res.send(category);
+    }
+
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 });
 
 app.listen(port, () => {
